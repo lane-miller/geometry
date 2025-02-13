e5ee7efb74b31da4af2bcff3aa45959adb5aeff5
Lane Miller <millerlanep@gmail.com>
2025-02-13 07:06:30 -0500

    Add vd25.py

---
+++ vd25.py
@@ -0,0 +1,23 @@
+import numpy as np
+from matplotlib.patches import Rectangle, Circle
+
+class Clover:
+	def __init__(self, c: tuple, x: float, d: float):
+		self.c = c
+		self.x = x
+		self.d = d
+		self.rect_data = []
+		self.circ_data = []
+
+	def generate(self):
+		# Quadrant transforms
+		quadX = [(1., 1.), 
+				 (-1., 1.),
+				 (-1., -1),
+				 (1. -1.)]
+	    
+
+		return
+
+	def draw(self):
+		return
